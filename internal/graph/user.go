d1ec1746a346bb61ea7117439fcafdab70e3c8b6
Johnny Horvi <johnny@horvi.no>
2023-04-20 12:57:17 +0200

    Add missing file

---
+++ internal/graph/user.go
@@ -0,0 +1,26 @@
+package graph
+
+import (
+	"github.com/nais/console-backend/internal/console"
+	"github.com/nais/console-backend/internal/graph/model"
+)
+
+func edges(teams []console.TeamMembership, first int, after int) []*model.TeamEdge {
+	edges := []*model.TeamEdge{}
+	limit := first + after
+	if limit > len(teams) {
+		limit = len(teams)
+	}
+	for i := after; i < limit; i++ {
+		team := teams[i].Team
+		edges = append(edges, &model.TeamEdge{
+			Cursor: model.Cursor{Offset: i + 1},
+			Node: &model.Team{
+				ID:          team.Slug,
+				Name:        team.Slug,
+				Description: &team.Purpose,
+			},
+		})
+	}
+	return edges
+}
