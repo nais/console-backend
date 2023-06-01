aca8b843ba33cc29ca61f7e24f63ef8c5d8ca3dc
Roger Bjørnstad <roger.bjornstad@nav.no>
2023-06-01 15:47:45 +0200

    adding debug-log for k8s.App - something is fishy

--- internal/k8s/k8s.go
+++ internal/k8s/k8s.go
@@ -118,6 +118,7 @@ func (c *Client) Run(ctx context.Context) {
 }
 
 func (c *Client) App(ctx context.Context, name, team, env string) (*model.App, error) {
+	c.log.Debugf("getting app %q in namespace %q in env %q", name, team, env)
 	obj, err := c.informers[env].AppInformer.Lister().ByNamespace(team).Get(name)
 	if err != nil {
 		return nil, fmt.Errorf("getting application: %w", err)
