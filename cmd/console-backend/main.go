0da52150edd38c81e81091878588d93d1e2bb6af
Christer Edvartsen <christer.edvartsen@nav.no>
2023-09-05 07:55:47 +0200

    create separate config package to minimize main

--- cmd/console-backend/main.go
+++ cmd/console-backend/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"net/http"
-	"os"
-	"strings"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
@@ -13,6 +11,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/nais/console-backend/internal/auth"
+	"github.com/nais/console-backend/internal/config"
 	"github.com/nais/console-backend/internal/graph"
 	"github.com/nais/console-backend/internal/hookd"
 	"github.com/nais/console-backend/internal/k8s"
@@ -21,44 +20,9 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/cors"
 	"github.com/sirupsen/logrus"
-	flag "github.com/spf13/pflag"
 	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/sdk/metric"
 )
-
-type Config struct {
-	Audience                 string
-	BindHost                 string
-	FieldSelector            string
-	HookdEndpoint            string
-	HookdPSK                 string
-	LogLevel                 string
-	Port                     string
-	RunAsUser                string
-	TeamsEndpoint            string
-	TeamsToken               string
-	Tenant                   string
-	KubernetesClusters       []string
-	KubernetesClustersStatic []string
-}
-
-var cfg = &Config{}
-
-func init() {
-	flag.StringVar(&cfg.Audience, "audience", os.Getenv("IAP_AUDIENCE"), "IAP audience")
-	flag.StringVar(&cfg.BindHost, "bind-host", os.Getenv("BIND_HOST"), "Bind host")
-	flag.StringVar(&cfg.TeamsEndpoint, "teams-endpoint", envOrDefault("TEAMS_ENDPOINT", "http://teams-backend/query"), "Teams endpoint")
-	flag.StringVar(&cfg.TeamsToken, "teams-token", envOrDefault("TEAMS_TOKEN", "secret-admin-api-key"), "Teams token")
-	flag.StringVar(&cfg.HookdEndpoint, "hookd-endpoint", envOrDefault("HOOKD_ENDPOINT", "http://hookd"), "Hookd endpoint")
-	flag.StringVar(&cfg.HookdPSK, "hookd-psk", envOrDefault("HOOKD_PSK", "secret-frontend-psk"), "Hookd PSK")
-	flag.StringVar(&cfg.LogLevel, "log-level", "info", "which log level to output")
-	flag.StringVar(&cfg.Port, "port", envOrDefault("PORT", "8080"), "Port to listen on")
-	flag.StringVar(&cfg.Tenant, "tenant", envOrDefault("TENANT", "dev-nais"), "Which tenant we are running in")
-	flag.StringVar(&cfg.RunAsUser, "run-as-user", os.Getenv("RUN_AS_USER"), "Statically configured frontend user")
-	flag.StringVar(&cfg.FieldSelector, "field-selector", os.Getenv("FIELD_SELECTOR"), "Field selector for k8s resources")
-	flag.StringSliceVar(&cfg.KubernetesClusters, "kubernetes-clusters", splitEnv("KUBERNETES_CLUSTERS", ","), "Kubernetes clusters to watch (comma separated)")
-	flag.StringSliceVar(&cfg.KubernetesClustersStatic, "kubernetes-clusters-static", splitEnv("KUBERNETES_CLUSTERS_STATIC", ","), "Kubernetes clusters to watch with static credentials (comma separated entries on the format 'name|apiserver-host|token')")
-}
 
 func newServer(es graphql.ExecutableSchema) *handler.Server {
 	srv := handler.New(es)
@@ -77,10 +41,10 @@ func newServer(es graphql.ExecutableSchema) *handler.Server {
 }
 
 func main() {
-	flag.Parse()
-	log := newLogger()
 	ctx := context.Background()
+	cfg := config.New()
 
+	log := newLogger(cfg.LogLevel)
 	exporter, err := prometheus.New()
 	if err != nil {
 		log.Fatal(err)
@@ -143,25 +107,11 @@ func main() {
 	log.Fatal(http.ListenAndServe(cfg.BindHost+":"+cfg.Port, nil))
 }
 
-func envOrDefault(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-		return value
-	}
-	return fallback
-}
-
-func splitEnv(key, sep string) []string {
-	if value, ok := os.LookupEnv(key); ok {
-		return strings.Split(value, sep)
-	}
-	return nil
-}
-
-func newLogger() *logrus.Logger {
+func newLogger(logLevel string) *logrus.Logger {
 	log := logrus.StandardLogger()
 	log.SetFormatter(&logrus.JSONFormatter{})
 
-	l, err := logrus.ParseLevel(cfg.LogLevel)
+	l, err := logrus.ParseLevel(logLevel)
 	if err != nil {
 		log.Fatal(err)
 	}
---
+++ internal/config/config.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"os"
+	"strings"
+
+	flag "github.com/spf13/pflag"
+)
+
+type Config struct {
+	Audience                 string
+	BindHost                 string
+	FieldSelector            string
+	HookdEndpoint            string
+	HookdPSK                 string
+	LogLevel                 string
+	Port                     string
+	RunAsUser                string
+	TeamsEndpoint            string
+	TeamsToken               string
+	Tenant                   string
+	KubernetesClusters       []string
+	KubernetesClustersStatic []string
+}
+
+func New() *Config {
+	cfg := &Config{}
+
+	flag.StringVar(&cfg.Audience, "audience", os.Getenv("IAP_AUDIENCE"), "IAP audience")
+	flag.StringVar(&cfg.BindHost, "bind-host", os.Getenv("BIND_HOST"), "Bind host")
+	flag.StringVar(&cfg.TeamsEndpoint, "teams-endpoint", envOrDefault("TEAMS_ENDPOINT", "http://teams-backend/query"), "Teams endpoint")
+	flag.StringVar(&cfg.TeamsToken, "teams-token", envOrDefault("TEAMS_TOKEN", "secret-admin-api-key"), "Teams token")
+	flag.StringVar(&cfg.HookdEndpoint, "hookd-endpoint", envOrDefault("HOOKD_ENDPOINT", "http://hookd"), "Hookd endpoint")
+	flag.StringVar(&cfg.HookdPSK, "hookd-psk", envOrDefault("HOOKD_PSK", "secret-frontend-psk"), "Hookd PSK")
+	flag.StringVar(&cfg.LogLevel, "log-level", "info", "which log level to output")
+	flag.StringVar(&cfg.Port, "port", envOrDefault("PORT", "8080"), "Port to listen on")
+	flag.StringVar(&cfg.Tenant, "tenant", envOrDefault("TENANT", "dev-nais"), "Which tenant we are running in")
+	flag.StringVar(&cfg.RunAsUser, "run-as-user", os.Getenv("RUN_AS_USER"), "Statically configured frontend user")
+	flag.StringVar(&cfg.FieldSelector, "field-selector", os.Getenv("FIELD_SELECTOR"), "Field selector for k8s resources")
+	flag.StringSliceVar(&cfg.KubernetesClusters, "kubernetes-clusters", splitEnv("KUBERNETES_CLUSTERS", ","), "Kubernetes clusters to watch (comma separated)")
+	flag.StringSliceVar(&cfg.KubernetesClustersStatic, "kubernetes-clusters-static", splitEnv("KUBERNETES_CLUSTERS_STATIC", ","), "Kubernetes clusters to watch with static credentials (comma separated entries on the format 'name|apiserver-host|token')")
+	flag.Parse()
+
+	return cfg
+}
+
+func envOrDefault(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
+func splitEnv(key, sep string) []string {
+	if value, ok := os.LookupEnv(key); ok {
+		return strings.Split(value, sep)
+	}
+	return nil
+}
