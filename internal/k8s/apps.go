def82a15015665918098b61f88a70bb6da0e6525
Roger Bjørnstad <roger.bjornstad@nav.no>
2023-09-21 09:01:27 +0200

    remove old comment

--- internal/k8s/apps.go
+++ internal/k8s/apps.go
@@ -28,33 +28,6 @@ const (
 	AppConditionUnknown               AppCondition = "Unknown"
 )
 
-/*
-cluster:
-	dev-fss:
-	  - ".adeo.no"
-	  - ".intern.dev.adeo.no"
-	  - ".dev-fss.nais.io"
-	  - ".dev.adeo.no"
-	  - ".dev.intern.nav.no"
-	  - ".nais.preprod.local"
-	dev-gcp:
-	  - ".dev-gcp.nais.io"
-	  - ".dev.intern.nav.no"
-	  - ".dev.nav.no"
-	  - ".intern.nav.no"
-	  - ".dev.adeo.no"
-	  - ".labs.nais.io"
-	  - ".ekstern.dev.nais.io"
-	prod-fss:
-	  - ".adeo.no"
-	  - ".nais.adeo.no"
-	  - ".prod-fss.nais.io"
-	prod-gcp:
-	  - ".dev.intern.nav.no"
-	  - ".prod-gcp.nais.io"
-}
-*/
-
 func getDeprecatedIngresses(cluster string) []string {
 	deprecatedIngresses := map[string][]string{
 		"dev-fss": {
