2e7dca44deb0d7e1d6be7560bd55e31eec196b5f
Christer Edvartsen <christer.edvartsen@nav.no>
2023-10-07 19:19:44 +0200

    Revert "don't modify the dsn as the migration connection does not seem to handle that very well"
    This reverts commit d1d90d0813972639521421f39c6a41e2941b3c35.

--- internal/database/database.go
+++ internal/database/database.go
@@ -53,7 +53,7 @@ func NewDB(ctx context.Context, dsn string, log *logrus.Entry) (*gensql.Queries,
 		closeFuncs = append(closeFuncs, dialer.Close)
 
 		var instanceConnectionName string
-		instanceConnectionName, err = GetInstanceConnectionNameFromDsn(dsn)
+		dsn, instanceConnectionName, err = ExtractInstanceConnectionNameFromDsn(dsn)
 		if err != nil {
 			return nil, closeFuncs, err
 		}
@@ -84,17 +84,24 @@ func NewDB(ctx context.Context, dsn string, log *logrus.Entry) (*gensql.Queries,
 	return gensql.New(conn), closeFuncs, nil
 }
 
-// GetInstanceConnectionNameFromDsn will get the instance connection name from the dsn.
-func GetInstanceConnectionNameFromDsn(dsn string) (string, error) {
+// ExtractInstanceConnectionNameFromDsn will extract the instance connection name from the dsn and return the modified
+// dsn, the instance connection name and a potential error. On error, the first two return values are both empty strings.
+func ExtractInstanceConnectionNameFromDsn(dsn string) (string, string, error) {
 	parts, err := url.ParseQuery(strings.ReplaceAll(dsn, " ", "&"))
 	if err != nil {
-		return "", err
+		return "", "", err
 	}
 	instanceConnectionName := parts.Get("host")
 	if instanceConnectionName == "" {
-		return "", fmt.Errorf("dsn does not have a host field: %q", dsn)
+		return "", "", fmt.Errorf("dsn does not have a host field: %q", dsn)
 	}
-	return instanceConnectionName, nil
+	delete(parts, "host")
+	s, err := url.PathUnescape(parts.Encode())
+	if err != nil {
+		return "", "", err
+	}
+	updatedDsn := strings.ReplaceAll(s, "&", " ")
+	return updatedDsn, instanceConnectionName, nil
 }
 
 // migrateDatabaseSchema runs database migrations
