6c7ee502430930c3df84a898698306ff595409da
Christer Edvartsen <christer.edvartsen@nav.no>
2023-10-24 15:49:10 +0200

    return JSON-object with error

--- internal/auth/iap.go
+++ internal/auth/iap.go
@@ -10,20 +10,24 @@ import (
 	"google.golang.org/api/idtoken"
 )
 
-type contextKey int
-
 const contextEmail contextKey = 1
 
-type Middleware func(next http.Handler) http.Handler
+type (
+	contextKey int
+	Middleware func(next http.Handler) http.Handler
+)
 
-func StaticUser(user string) Middleware {
+// StaticUser returns a middleware that sets the email address of the authenticated user to the given value
+func StaticUser(email string) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), contextEmail, user)))
+			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), contextEmail, email)))
 		})
 	}
 }
 
+// ValidateIAPJWT returns a middleware that validates the X-Goog-IAP-JWT-Assertion header and sets the email address of
+// the authenticated user to the value of the X-Goog-Authenticated-User-Email header
 func ValidateIAPJWT(aud string) Middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -31,25 +35,24 @@ func ValidateIAPJWT(aud string) Middleware {
 
 			payload, err := idtoken.Validate(r.Context(), iapJWT, aud)
 			if err != nil {
-				http.Error(w, "Invalid JWT token", http.StatusUnauthorized)
+				http.Error(w, jsonError("Invalid JWT token"), http.StatusUnauthorized)
 				return
 			}
 
 			if time.Unix(payload.IssuedAt, 0).After(time.Now().Add(30 * time.Second)) {
-				http.Error(w, "JWT token is in the future", http.StatusUnauthorized)
+				http.Error(w, jsonError("JWT token is in the future"), http.StatusUnauthorized)
 				return
 			}
 
 			if payload.Issuer != "https://cloud.google.com/iap" {
-				http.Error(w, "Invalid JWT token issuer", http.StatusUnauthorized)
+				http.Error(w, jsonError("Invalid JWT token issuer"), http.StatusUnauthorized)
 				return
 			}
 
 			email := r.Header.Get("X-Goog-Authenticated-User-Email")
 			_, email, _ = strings.Cut(email, ":")
 
-			ctx := context.WithValue(r.Context(), contextEmail, email)
-			next.ServeHTTP(w, r.WithContext(ctx))
+			next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), contextEmail, email)))
 		})
 	}
 }
@@ -61,4 +64,9 @@ func GetEmail(ctx context.Context) (string, error) {
 		return "", fmt.Errorf("no email in context")
 	}
 	return email, nil
+}
+
+// jsonError returns a JSON error message
+func jsonError(msg string) string {
+	return fmt.Sprintf(`{"error": "%s"}`, msg)
 }
