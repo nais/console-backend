01a4f4217bb7c1277335f8271710c1f119c179b2
Christer Edvartsen <christer.edvartsen@nav.no>
2023-10-25 23:04:51 +0200

    let gqlgen handle the model.Instance type

--- internal/graph/model/instance.go
+++ internal/graph/model/instance.go
@@ -1,25 +1,7 @@
 package model
 
-import (
-	"time"
-)
-
 type InstanceGQLVars struct {
 	Env     string
 	Team    string
 	AppName string
 }
-
-type Instance struct {
-	ID       Ident           `json:"id"`
-	Name     string          `json:"name"`
-	Image    string          `json:"image"`
-	State    InstanceState   `json:"state"`
-	Restarts int             `json:"restarts"`
-	Message  string          `json:"message"`
-	Created  time.Time       `json:"created"`
-	GQLVars  InstanceGQLVars `json:"-"`
-}
-
-func (Instance) IsNode()        {}
-func (i Instance) GetID() Ident { return i.ID }
